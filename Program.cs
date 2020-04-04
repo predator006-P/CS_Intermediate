9a9db5978d92e5c969253c966a0338ca1e3fb4c6
pnemeth <pnemeth@LT000305>
2020-04-04 15:30:10 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS_IntermediateCourse
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
